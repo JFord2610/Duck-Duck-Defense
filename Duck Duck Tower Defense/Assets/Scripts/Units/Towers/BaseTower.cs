4941a9fb3b8b91a879d8fc7e9238b327d93c31c0
John Ford <JFord2610@Gmail.com>
2019-10-03 01:41:20 -0400

    Added mouse interactions to towers

--- Duck Duck Tower Defense/Assets/Scripts/Units/Towers/BaseTower.cs
+++ Duck Duck Tower Defense/Assets/Scripts/Units/Towers/BaseTower.cs
@@ -5,20 +5,24 @@ using System.Collections;
 public abstract class BaseTower : MonoBehaviour
 {
     public float damage = 0, attackRange = 0, attackSpeed = 0;
+    [SerializeField] float scaleFactor = 0.15f;
 
     protected bool onCooldown = false;
+    protected bool hovering = false;
     public bool alive = false;
     public ETargetingType targetingType = ETargetingType.Closest;
-    
+
     public bool colliding
     {
         get { return collisions > 0; }
     }
-    private int collisions;
+    private int collisions = 0;
 
     protected GameManagerScript gameManager = null;
-    protected Transform sprite;
-    protected Animator anim;
+    protected GameObject attackRadiusObj = null;
+    protected Transform spriteTransform = null;
+    protected SpriteRenderer sprite = null;
+    protected Animator anim = null;
 
     private void Start()
     {
@@ -28,9 +32,12 @@ public abstract class BaseTower : MonoBehaviour
     protected virtual void Init()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
-        sprite = transform.GetChild(0);
-        anim = sprite.gameObject.GetComponent<Animator>();
+        spriteTransform = transform.GetChild(0);
+        sprite = spriteTransform.gameObject.GetComponent<SpriteRenderer>();
+        anim = spriteTransform.gameObject.GetComponent<Animator>();
         anim.speed = 1 / attackSpeed;
+        attackRadiusObj = transform.GetChild(2).gameObject;
+        attackRadiusObj.GetComponent<SpriteRenderer>().size = new Vector2(attackRange * 2, attackRange * 2);
     }
 
     private void Update()
@@ -120,9 +127,9 @@ public abstract class BaseTower : MonoBehaviour
 
     protected void RotateToPoint(Vector3 p)
     {
-        Vector3 dir = p - sprite.position;
+        Vector3 dir = p - spriteTransform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        sprite.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        spriteTransform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -133,5 +140,37 @@ public abstract class BaseTower : MonoBehaviour
     private void OnCollisionExit2D(Collision2D collision)
     {
         collisions--;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!alive) return;
+        hovering = true;
+        StartCoroutine("ScaleUp");
+        attackRadiusObj.SetActive(true);
+    }
+    IEnumerator ScaleUp()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            spriteTransform.localScale += new Vector3(scaleFactor / 5, scaleFactor / 5, 0.0f);
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (!alive) return;
+        hovering = false;
+        StartCoroutine("ScaleDown");
+        attackRadiusObj.SetActive(false);
+    }
+    IEnumerator ScaleDown()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            spriteTransform.localScale -= new Vector3(scaleFactor / 5, scaleFactor / 5, 0.0f);
+            yield return new WaitForSeconds(0.005f);
+        }
     }
 }
