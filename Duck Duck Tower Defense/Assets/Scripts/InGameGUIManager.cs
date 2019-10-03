6de511e643179ad6267f9ecc19c4d6910a1bc468
John Ford <JFord2610@Gmail.com>
2019-10-02 23:47:37 -0400

    Added in-game options menu

---
+++ Duck Duck Tower Defense/Assets/Scripts/InGameGUIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameGUIManager : MonoBehaviour
+{
+    GameObject canvas;
+    GameObject inGameGUIHolder;
+    GameObject optionsMenuHolder;
+    GameObject optionsButton;
+
+    private void Awake()
+    {
+        canvas = GameObject.Find("GameOverlayCanvas");
+        for (int i = 0; i < canvas.transform.childCount; i++)
+        {
+            Transform child = canvas.transform.GetChild(i);
+            if (child.name == "InGameGUIHolder")
+            {
+                inGameGUIHolder = child.gameObject;
+                for (int k = 0; k < child.childCount; k++)
+                {
+                    GameObject obj = child.GetChild(i).gameObject;
+                    switch (obj.name)
+                    {
+                        case "OptionsButton":
+                            optionsButton = obj;
+                            break;
+                    }
+                }
+            }
+            else if (child.name == "OptionsMenuHolder")
+            {
+                optionsMenuHolder = child.gameObject;
+            }
+        }
+    }
+
+    public void OptionsButton_OnClick()
+    {
+        inGameGUIHolder.SetActive(false);
+        //To Do: Pause game??
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void ReturnToGameButton_Click()
+    {
+        optionsMenuHolder.SetActive(false);
+        //To Do: UnPause game??
+        inGameGUIHolder.SetActive(true);
+    }
+}
