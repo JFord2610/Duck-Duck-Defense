d8feeba554578c98907253d54116fdfdec89337a
John Ford <JFord2610@Gmail.com>
2019-10-14 16:32:47 -0400

    Added agent quack sprite and animations

---
+++ Duck Duck Tower Defense/Assets/Scripts/Units/Geese/GooseInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Geese/Goose Info")]
+public class GooseInfo : ScriptableObject
+{
+    public string gooseName;
+    public GooseStats stats;
+    public Sprite sprite;
+
+    public GooseInfo Createcopy()
+    {
+        GooseInfo newInfo = CreateInstance<GooseInfo>();
+        newInfo.gooseName = gooseName;
+        newInfo.stats = stats.CreateCopy();
+        newInfo.sprite = sprite;
+        return newInfo;
+    }
+}
